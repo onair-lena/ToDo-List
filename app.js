9a0cb660916a3c9c3fcbb7cdc6a6df8cfa0fc126
onair-lena <pechenko@bk.ru>
2023-06-16 20:19:07 +0300

    ToDo component added

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import { Note } from './Note.js';
 import { NoteList } from './NoteList.js';
+import { ToDo } from './ToDo.js';
 
-let newList = new NoteList(document.getElementById('app'), 'noteList', [
-  { name: 'Task 1' },
-  { name: 'Task 2' },
-]);
+let app = new ToDo(document.getElementById('app'));
+app.addUser('My ToDo List', 'my');
+app.addUser('My Shopping List', 'shop');
+app.addUser('My Cooking List', 'cooking');
+
+// let newList = new NoteList(document.getElementById('app'), 'noteList', [
+//   { name: 'Task 1' },
+//   { name: 'Task 2' },
+// ]);
 
 // let initialNote = new Note(document.getElementById('app'), 'Hello');
 
 document.getElementById('action').addEventListener('click', () => {
-  newList.add(prompt('Add new task'));
+  app.addUser(prompt('Add list title'));
 });
